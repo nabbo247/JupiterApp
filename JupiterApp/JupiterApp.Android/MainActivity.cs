88ef1cc360c4ff86fe954ec7151ce6181c270649
NABIL ABUBAKAR <NA01190@jaizbankplc.com>
2023-10-21 22:18:49 +0100

    first commit

--- JupiterApp/JupiterApp.Android/MainActivity.cs
+++ JupiterApp/JupiterApp.Android/MainActivity.cs
@@ -5,7 +5,7 @@ using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
 
-namespace JupiterApp.Droid
+namespace JupiterApp.Droid 
 {
     [Activity(Label = "JupiterApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
